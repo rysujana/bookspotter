fcb43978d2a70912a8397633760be96a5aa2139b
ana117 <63540951+ana117@users.noreply.github.com>
2023-12-08 20:12:23 +0700

    feat: add book detail

--- core/util.py
+++ core/util.py
@@ -27,11 +27,11 @@ def process_query_result(result):
                 dct[key] = value["value"]
 
         results.append(dct)
+
     return results
 
 
-def query_graph(title=None, author=None, sort_by=None, limit=12, offset=0, is_random=False):
-    
+def query_graph_min(title=None, author=None, sort_by=None, limit=12, offset=0, is_random=False):
     or_filters = []
     if title:
         title = title.replace('"', '\\"')
@@ -78,6 +78,61 @@ def query_graph(title=None, author=None, sort_by=None, limit=12, offset=0, is_ra
     {sorting_query}
     limit {limit}
     offset {offset}
+    """
+
+    sparql.setQuery(query)
+    result = sparql.queryAndConvert()
+    return process_query_result(result)
+
+
+def query_graph_iri(iri):
+    query = PREFIX + f"""
+    select ?book_iri ?title (group_concat(distinct ?author_name; SEPARATOR=", ") AS ?authors) ?image ?link ?publisher ?publish_date ?dim_unit ?depth_value ?width_value ?length_value ?weight_unit ?weight_value ?isbn ?avg_rating ?n_reviews ?link ?price where {{
+        ?book_iri rdf:type :Book ;
+            rdfs:label ?title .
+        
+        optional {{
+            ?book_iri prop:written_by ?author .
+            ?author rdf:type :Author ;
+                prop:name ?author_name .
+        }} .
+        optional {{ ?book_iri prop:link ?link }} .
+        optional {{ 
+            ?book_iri prop:published_by ?published_by .
+            ?published_by rdf:type :Publisher ;
+                rdfs:label ?publisher
+        }} .
+        optional {{ ?book_iri prop:publish_date ?publish_date }} .
+        optional {{ 
+            ?book_iri prop:depth [
+                prop:unit ?dim_unit ;
+                prop:value ?depth_value
+            ] .    
+        }} .
+        optional {{ ?book_iri prop:width [ prop:value ?width_value ] }} .
+        optional {{ ?book_iri prop:length [ prop:value ?length_value ] }} .
+        optional {{ 
+            ?book_iri prop:weight [
+                prop:unit ?weight_unit ;
+                prop:value ?wight_value
+            ] .    
+        }} .
+        optional {{ ?book_iri prop:isbn ?isbn }} .
+        optional {{ 
+            ?book_iri prop:reviews [
+                prop:avg_reviews ?avg_rating ;
+                prop:n_reviews ?n_reviews
+            ] 
+        }} .
+        optional {{ ?book_iri prop:image ?image }} .
+        optional {{ 
+            ?book_iri prop:price [
+                prop:new_price ?price ;
+            ] 
+        }} .
+
+        filter(?book_iri = :{iri})
+    }} group by ?book_iri ?title ?image ?link ?publisher ?publish_date ?dim_unit ?depth_value ?width_value ?length_value ?weight_unit ?weight_value ?isbn ?avg_rating ?n_reviews ?link ?price
     """
 
     sparql.setQuery(query)
--- core/views.py
+++ core/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .util import query_graph
+from .util import query_graph_iri, query_graph_min
 
 
 def home(request):
     sort_by = request.GET.get('sort')
     
-    random_books = query_graph(limit=3, is_random=True)
-    books = query_graph(sort_by=sort_by)
+    random_books = query_graph_min(limit=3, is_random=True)
+    books = query_graph_min(sort_by=sort_by)
 
     return render(request, 'home.html', {'random_books': random_books, 'books': books})
 
@@ -16,14 +16,15 @@ def search_results(request):
     search_query = request.GET.get('query')
     sort_by = request.GET.get('sort')
 
-    books = query_graph(title=search_query, author=search_query, sort_by=sort_by)
+    books = query_graph_min(title=search_query, author=search_query, sort_by=sort_by)
 
     query_get_parameters = "+".join(search_query.split())
     return render(request, 'search_results.html', {'query_result': books, 'query_param': query_get_parameters})
 
 
-def book_detail(request):
-    return render(request, 'book_detail.html')
+def book_detail(request, iri):
+    book = query_graph_iri(iri)[0]
+    return render(request, 'book_detail.html', {'book': book})
 
 
 def load_more_books(request):
@@ -32,7 +33,7 @@ def load_more_books(request):
     offset = int(request.GET.get('offset'))
     limit = 12
 
-    books = query_graph(title=query, author=query, sort_by=sort_by, offset=offset, limit=limit)
+    books = query_graph_min(title=query, author=query, sort_by=sort_by, offset=offset, limit=limit)
     end_of_data = False if len(books) == limit else True
 
     return JsonResponse({"books": books, "end_of_data": end_of_data})
